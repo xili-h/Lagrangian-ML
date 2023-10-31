d1d092a604d211eabbb573803477ced6368cdf9f
希理 <mcgogogo@qq.com>
2023-10-30 20:03:01 -0700

    Resnet

--- load_model.py
+++ load_model.py
@@ -1,40 +1,31 @@
 import torch
-from lagrangian import Net2, get_space_time_grid, DEVICE,get_psi
+from lagrangian import Net2, Net3, plot_net, DEVICE, get_psi, initial_wavepacket_3
 import matplotlib.pyplot as plt
 from Animation import Animation
 
-net = Net2()
-net.load_state_dict(torch.load("./lnet2_k_neg2_larger_inital.pth"))
-net.to(DEVICE)
+# net = Net2()
+#net.load_state_dict(torch.load("./bnet1_test1.pth"))
+# net.to(DEVICE)
 
+main_net = Net3(initial_wavepacket_3)
 
-# n_x=n_t=100
-# t_f=3
-# x_0=-10
-# space_period=20
-# space = torch.linspace(x_0, x_0+space_period, n_x)
-# x, dt, dx = get_space_time_grid(n_t, n_x)
+main_net.to(DEVICE)
+# boundary_net = Net1()
+# boundary_net.to(DEVICE)
 
-def plot_net(net, ax, n_t: int=100, n_x:int =100,
-            t_0=0, t_len=5,x_0=-10,space_period=20):
-    space = torch.linspace(x_0, x_0+space_period, n_x, device=DEVICE)
-    time = torch.linspace(t_0, t_0+t_len, n_t, device=DEVICE)
-    time, space = torch.meshgrid(time,space,indexing='ij')
-    x = torch.cat((time[:,:,None],space[:,:,None]),-1)
-    x = torch.flatten(x, end_dim=-2)
+#boundary_net.load_state_dict(torch.load("./bnet1_test1.pth"))
+main_net.load_state_dict(torch.load("./mnet.pth"))
 
-    psi = get_psi(net(x))
-    psi = psi.reshape(n_t,n_x)
-
-    psi2 = (torch.conj(psi)*psi).real
-    psi2 = psi2/torch.sum(psi2,dim=1)[:,None]
-
-    psi = psi.real/torch.sum(psi2,dim=1)[:,None]
-
-    ax.clear()
-    ax.plot_surface(time.cpu(),space.cpu(),psi.cpu())
-    plt.draw()
-    plt.pause(0.1)
+time_len = 5
+# def boundary_and_main(x):
+#     with torch.no_grad():
+#         #boundary = boundary_net(x)
+#         boundary = initial_wavepacket(x[:,1])[:, None]
+#     main = main_net(x)
+    
+#     time_grid = x[:,0][:, None]
+#     total = boundary*(1- time_grid/time_len)**2 + main*time_grid/time_len  #EQ2.6 of 2205.00593
+#     return total
 
 def run_animation(net, n_t: int=100, n_x:int =100,
             t_0=0, t_len=5,x_0=-10,space_period=20):
@@ -58,4 +49,21 @@ def run_animation(net, n_t: int=100, n_x:int =100,
     anim.run_animtion(-50)
 
 with torch.no_grad():
-    run_animation(net)
+    # run_animation(boundary_and_main)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    plot_net(main_net, ax)
+    plt.show()
+
+
+    # space = torch.linspace(-10, 10, 1000, device=DEVICE)
+    # x_at_time_0 = torch.cat((torch.zeros_like(space[:,None]),space[:,None]), dim=-1)
+    # psi = get_psi(boundary_and_main(x_at_time_0))
+    # dx = 20/(1000-1)
+    # psi2 = (torch.conj(psi)*psi).real
+    # N = dx*torch.sum(psi2)
+    # psi2 = (psi2/N).flatten()
+    # psi = (psi/N).flatten()
+    # plt.plot(space.cpu(), psi.imag.cpu()-initial_wavepacket(space).imag.cpu())
+    # plt.show()
